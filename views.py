c7d221ff5722661ca900314b160179ad8a0a8092
lbonilla-create <lbonilla@gbox.adnu.edu.ph>
2026-04-22 16:22:57 +0800

    Django Records and Template

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
-def members(request) :
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
-
-# Create your views here.
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+    'mymembers': mymembers,
+}
+    return HttpResponse(template.render(context,
+request))
