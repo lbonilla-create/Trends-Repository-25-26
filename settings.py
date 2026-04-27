bf0ef45cd65270750a321fc03b4c807afe0e870d
lbonilla-create <lbonilla@gbox.adnu.edu.ph>
2026-04-27 15:35:32 +0800

    Custom 404 Template

--- settings.py
+++ settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4er$ase9^)(aad5-&-*0$iqc((xllmi3md!ak=@lsjcd&q^+@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
